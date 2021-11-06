275df1239c21c055defd04700b56432b5980ed8a
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-11-06 22:23:49 +0900

    表示画面vue+viteバージョン

---
+++ front-vue/src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>記事特徴抽出ツール</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+<style scoped>
+header {
+    text-align: center;
+    height: 65px;
+}
+h1 {
+    font-family: 'Rampart One', cursive;
+    font-size: 32px;
+}
+</style>
